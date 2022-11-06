458d856fccd313253e4e0860f3715d7c108cbba4
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-11-06 15:34:55 +0100

    completion

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+      <p class="red">{{ saluto }}</p>
+      <div v-for="(item, index) in test" :key="index">
+          {{ item }}
+        <AppTitleComponent/>
+      </div>
+  </div>
+</template>
+
+<script>
+import AppTitleComponent from './components/AppTitleComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTitleComponent
+   
+  },
+  data() {
+    return {
+      saluto: 'Hello World!',
+      test: [1, 2, 4, 8, 16, 32],
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AppTitleComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="red" @click="saluta">
+        My First Component
+        <div class="inner">Sono Inner</div>
+    </div>
+    <input class="red active" type="text" v-model="text">
+    <div class="inner">Inner Fuori da Red</div>
+    <div class="red-square"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTitleComponent',
+    data() {
+        return {
+            text: '',
+        };
+    },
+    methods: {
+        saluta() {
+            console.log("Hello from your first component");
+        },
+    },
+}
+</script>
+
+<style scoped lang="scss">
+$bgColor: red;
+
+
+@mixin ac-square {
+    width: 150px;
+    height: 150px;
+}
+
+.red-square{
+    @include ac-square();
+    background-color: brown;
+}
+
+.red {
+    background-color: red;
+}
+
+.red .inner{
+    background-color: yellowgreen;
+}
+
+.red.active{
+    background-color: blue;
+}
+
+.inner{
+     background-color: yellow;
+}
+</style>
